3ec336d045cb1e041839423f8a147810f57179cc
Alec Bakholdin <alecbakholdin@gmail.com>
2022-04-28 20:28:49 -0400

    made resource icons look slightly better

--- src/Game/Cards/CardBase.tsx
+++ src/Game/Cards/CardBase.tsx
@@ -1,17 +1,26 @@
 import { Box } from "@mui/material";
-import {ReactElement} from "react";
+import { ReactElement } from "react";
 
-export function CardBase({children}: {children: ReactElement | ReactElement[]}): ReactElement {
-  return <Box
-    borderRadius={2}
-    className={"center-box"}
-    bgcolor={"background.paper"}
-    sx={{
-      border: "1px solid",
-      width: 350,
-      padding: 1,
-    }}
-  >{children}</Box>;
+export function CardBase({
+  children,
+}: {
+  children: ReactElement | ReactElement[];
+}): ReactElement {
+  return (
+    <Box
+      borderRadius={2}
+      className={"center-box"}
+      bgcolor={"background.paper"}
+      sx={{
+        border: "1px solid",
+        width: 350,
+        height: 200,
+        padding: 1,
+      }}
+    >
+      {children}
+    </Box>
+  );
 }
 
 export default CardBase;
--- src/Game/Cards/ResourceIcon/ResourceIcon.tsx
+++ src/Game/Cards/ResourceIcon/ResourceIcon.tsx
@@ -5,7 +5,7 @@ import {
 } from "../../../assets/types/Resources";
 import { Box, Icon, Typography } from "@mui/material";
 
-import "./ResourceIcon.css";
+import "./ResourceIcon.scss";
 
 export function ResourceIcon({
   type,
@@ -15,20 +15,30 @@ export function ResourceIcon({
   qty?: number;
 }): ReactElement {
   const color = type === "white" || type === "yellow" ? "black" : "white";
+  const boxSize = 27;
 
   return (
     <Box
-      width={15}
-      height={15}
+      height={boxSize}
+      width={boxSize}
       className={"resource-icon-root"}
       overflow={"hidden"}
     >
-      <Icon className={"resource-icon-icon"}>{getResourceSprite(type)}</Icon>
+      <Icon
+        className={"resource-icon-icon"}
+        sx={{ fontSize: `${boxSize * 0.9}px !important` }}
+      >
+        {getResourceSprite(type)}
+      </Icon>
       {qty && qty > 1 && (
         <Typography
           component={"span"}
           className={"resource-icon-count"}
           color={color}
+          sx={{
+            marginTop: `${boxSize * 0.19}px !important`,
+            fontSize: `${boxSize * 0.8}px !important`,
+          }}
         >
           {qty}
         </Typography>
