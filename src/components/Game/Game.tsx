b183ad4e99a526211f71588e8d9fb6aa2d1f2c60
Alec Bakholdin <alecbakholdin@gmail.com>
2022-05-05 19:23:44 -0400

    basic draft of ui

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,15 +1,19 @@
 import { ReactElement, useEffect } from "react";
-import { Drawer } from "@mui/material";
+import { Box, Drawer, Grid, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "redux/hooks";
 import { rejoinGame } from "redux/reducers/gameState";
 
 import "./Game.scss";
+import PlayerResources from "./PlayerResources/PlayerResources";
 
 export const Game = function (): ReactElement {
   const dispatch = useAppDispatch();
   const { playerId, playerName, isFresh, gameState } = useAppSelector(
     (state) => state.gameState
   );
+  const player = gameState.players[playerId ?? ""];
+
+  // rejoin game if cookies are set and game has not been fetched yet
   useEffect(() => {
     if (!isFresh && playerId && playerName) {
       dispatch(rejoinGame({ playerId, playerName }));
@@ -17,16 +21,32 @@ export const Game = function (): ReactElement {
   });
 
   return (
-    <>
-      <Drawer
-        container={() => window?.document.body}
-        variant={"permanent"}
-        ModalProps={{ keepMounted: true }}
-      >
-        testing
-      </Drawer>
-      {JSON.stringify(gameState)}
-    </>
+    <Box sx={{ height: "100vh" }}>
+      <Grid container height={"100%"}>
+        <Grid item container xs={3} direction={"column"}>
+          <Grid item width={"100%"} xs={3} className={"center-box"}>
+            {isFresh && (
+              <Typography>
+                turn: {gameState.turn}, phase: {gameState.phase}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item width={"100%"} xs={6} className={"center-box"}>
+            converter cards, colonies, research teams
+          </Grid>
+          <Grid item width={"100%"} xs={3}>
+            <Typography variant={"h4"}>My Resources</Typography>
+            <PlayerResources resources={player?.resources} />
+          </Grid>
+        </Grid>
+        <Grid item container xs={6} className={"center-box"}>
+          trading, confluence stuff
+        </Grid>
+        <Grid item container xs={3} className={"center-box"}>
+          players
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- src/components/Game/PlayerResources/PlayerResources.tsx
+++ src/components/Game/PlayerResources/PlayerResources.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import Resources from "assets/types/Resources";
 import ResourceIcon from "../Cards/ResourceIcon/ResourceIcon";
+import { Grid, Stack, Typography } from "@mui/material";
 
 export function PlayerResources({
   resources,
@@ -11,24 +12,24 @@ export function PlayerResources({
     return <></>;
   }
   return (
-    <>
-      <ResourceIcon type={"white"} qty={0} />
-      {resources.white}
-      <ResourceIcon type={"brown"} qty={0} />
-      {resources.brown}
-      <ResourceIcon type={"green"} qty={0} />
-      {resources.green}
-      <ResourceIcon type={"black"} qty={0} />
-      {resources.black}
-      <ResourceIcon type={"blue"} qty={0} />
-      {resources.blue}
-      <ResourceIcon type={"yellow"} qty={0} />
-      {resources.yellow}
-      <ResourceIcon type={"octagon"} qty={0} />
-      {resources.octagon}
-      <ResourceIcon type={"points"} qty={0} />
-      {resources.points}
-    </>
+    <Typography variant={"h6"} sx={{ width: "100%" }}>
+      <ResourceIcon type={"white"} />
+      {resources.white ?? 0}
+      <ResourceIcon type={"brown"} />
+      {resources.brown ?? 0}
+      <ResourceIcon type={"green"} />
+      {resources.green ?? 0}
+      <ResourceIcon type={"black"} />
+      {resources.black ?? 0}
+      <ResourceIcon type={"blue"} />
+      {resources.blue ?? 0}
+      <ResourceIcon type={"yellow"} />
+      {resources.yellow ?? 0}
+      <ResourceIcon type={"octagon"} />
+      {resources.octagon ?? 0}
+      <ResourceIcon type={"points"} />
+      {resources.points ?? 0}
+    </Typography>
   );
 }
 
